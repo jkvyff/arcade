4ccf9e412cd854fab78d1bb2e26b98c0e646754f
github username <github email address>
2019-06-17 12:09:44 -0700

    Added basic layout, and game proof of concept

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
+const GAMES = ['Rock Dodger', 'Helicopter',]
+
 document.addEventListener('DOMContentLoaded', (ev) => {
     console.log('DOM fully loaded');
     main() 
 });
 
 function main() {
-	console.log("Load gameList here.")
+	displayGamesList()
+	loadGame(0)
+}
+
+function displayGamesList() {
+	let gameList = document.getElementById('gameList');
+	for (let i = 0; i < GAMES.length; i++) {
+		displayGameInList(gameList, i);
+	}
+}
+
+function displayGameInList(parent, game) {
+	let li = document.createElement('li');
+
+	li.textContent = GAMES[game];
+
+	parent.appendChild(li);
+}
+
+function loadGame(gameNum) {
+	let game = document.getElementById('game')
+	switch(gameNum) {
+		case 0:
+			let a = document.createElement('a')
+			let div = document.createElement('div')
+			let script = document.createElement('script');   
+
+			a.id = "start"
+			a.href = "javascript:start()"
+			a.textContent = "START"
+			div.id = "dodger"
+			div.style.bottom = "0px"
+			div.style.left = "180px"
+			script.src = 'src/rockDodger.js';
+
+			game.appendChild(a)
+			game.appendChild(div)
+			game.appendChild(script)
+
+			link.href = 'style.css'
+			break;
+		default:
+	}
 }
